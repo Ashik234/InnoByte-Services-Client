0b9a7661c94eadfc0e107c435c92e3b3cf33cbdf
Ashik k <ashikash796@gmail.com>
2024-05-21 00:17:19 +0530

    feat(Auth):"Design Register Page"

---
+++ src/routes/UserRoutes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Register from "../pages/Register";
+function UserRoutes() {
+  return (
+    <Routes>
+      <Route exact path="/register" element={<Register />} />
+    </Routes>
+  )
+}
+
+export default UserRoutes
