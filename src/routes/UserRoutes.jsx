1baf43846edadd8533a9e0b4e00b76b81be7edfc
Ashik k <ashikash796@gmail.com>
2024-05-21 23:56:46 +0530

    refactor(Auth):Implement PublicRoutes

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import PublicRoutes from "./Protected/PublicRoutes";
+
 function UserRoutes() {
   return (
     <Routes>
-      <Route exact path="/register" element={<Register />} />
-      <Route exact path="/login" element={<Login />} />
+       <Route exact path="/register" element={<PublicRoutes><Register /></PublicRoutes>} />
+      <Route exact path="/login" element={<PublicRoutes><Login /></PublicRoutes>} />
     </Routes>
   )
 }
