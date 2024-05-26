f5d14efa38e050ed51ad7fd5ee30a88a728c54a3
Ashik k <ashikash796@gmail.com>
2024-05-26 23:38:56 +0530

    Added NotFoundPage

---
+++ src/pages/OtpVerify.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { Form, Input, Button } from "antd";
+import { toast } from "react-toastify";
+import userRequest from "../utils/userRequest"; 
+
+function OtpVerify({ email }) {
+  const navigate = useNavigate();
+  const [seconds, setSeconds] = useState(60);
+
+  const otpMutation = useMutation({
+    mutationFn: (data) => userRequest.post("/verify-otp", { ...data, email }),
+    onSuccess: (data) => {
+      localStorage.setItem("userJWT", data.token);
+      toast.success(data.message);
+      navigate("/");
+    },
+    onError: (error) => {
+      toast.error(error.response.data.message);
+    },
+  });
+
+  useEffect(() => {
+    if (seconds > 0) {
+      const timerId = setTimeout(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    }
+  }, [seconds]);
+
+  const onFinish = (formData) => {
+    otpMutation.mutate({ ...formData, email });
+  };
+
+  return (
+    <div className="flex justify-center bg-F9F9F9 p-4 sm:p-14 h-full">
+      <div className="bg-white lg:rounded-l-xl rounded-l-md border sm:w-1/2">
+        <h1 className="p-4">Enter OTP</h1>
+        <div className="lg:w-96 sm:w-80 mx-auto px-4">
+          <Form onFinish={onFinish} className="flex flex-col">
+            <Form.Item
+              name="otp"
+              rules={[
+                { required: true, pattern: /^\d{6}$/, message: "Enter a valid OTP." },
+              ]}
+            >
+              <Input size="large" placeholder="OTP" />
+            </Form.Item>
+            <Button type="primary" htmlType="submit" size="large">
+              Verify OTP
+            </Button>
+            <Button
+              disabled={seconds > 0}
+              type="primary"
+              htmlType="button"
+              size="large"
+              onClick={() => {
+                setSeconds(60);
+              }}
+            >
+              Re-send OTP {seconds > 0 ? `in ${seconds}s` : ""}
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OtpVerify;
